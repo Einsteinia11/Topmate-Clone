15e9861527d150bcc9ca300f9a2e72ae7ab022ab
Einsteinia11 <informationviakate2004@gmail.com>
2025-05-16 13:56:01 +0530

    Added HeroSection with responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/common/Navbar'
+import HeroSection from '../components/home-page/HeroSection';
 
 function Home() {
   return (
-    <div className="bg-[#0D0D0D] h-screen">
-      <Navbar />
+    <div className="h-screen">
+      <div className="bg-[#0D0D0D]">
+        <Navbar />
+        <HeroSection />
+      </div>
     </div>
   );
 }
